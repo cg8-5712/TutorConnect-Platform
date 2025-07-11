6197c48e47a1b8be51d2ff9313860be3bf760f7d
serine <serine.1984@outlook.com>
2025-07-11 17:47:24 +0800

    添加字体文件和全局样式导入

---
+++ client/src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+// 导入全局样式
+import '@/assets/styles/main.scss'
+
+createApp(App)
+  .use(router)
+  .mount('#app')
